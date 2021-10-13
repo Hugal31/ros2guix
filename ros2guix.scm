d2e7235e67183d7b5d4070c2b348a78199722aa9
Hugo Laloge <hla@lescompanions.com>
2021-10-13 15:03:11 +0200

    Extract imports, modules and print the result

--- ros2guix.scm
+++ ros2guix.scm
@@ -20,7 +20,7 @@
              (srfi srfi-43)
              (sxml simple)
              ((sxml xpath)
-              #:select (sxpath))
+              #:select (sxpath node-or))
              (yaml)
              (web uri))
 
@@ -42,16 +42,20 @@ Convert the given PACKAGES.\n")
 
 ;; Args
 (define %options
-  (list (option '(#\r "ros-distro") #t #f
-                (lambda (opt name arg result)
-                  (alist-cons 'ros-distro arg result)))
+  (list (option '(#\a "all") #f #f
+                (lambda (_1 _2 _3 result)
+                  (alist-cons 'all-packages #t result)))
+        (option '(#\r "ros-distro") #t #f
+                   (lambda (opt name arg result)
+                     (alist-cons 'ros-distro arg result)))
         (option '(#\h "help") #f #f
 	        (lambda _
 	          (show-help)))))
 
 (define %default-options
   ;; Alist of default option values.
-  `((ros-distro . ,(getenv "ROS_DISTRO"))
+  `((all-packages . #f)
+    (ros-distro . ,(getenv "ROS_DISTRO"))
     (packages . ())))
 
 (define-record-type <ros-package>
@@ -75,27 +79,43 @@ Convert the given PACKAGES.\n")
 
   (let* ((opts (parse-options))
          (ros-distro (assq-ref opts 'ros-distro))
-         (packages-to-convert (assq-ref opts 'packages)))
+         (packages-to-process (assq-ref opts 'packages))
+         (packages-pred
+          (cond
+           ((assq-ref opts 'all-packages)
+            (lambda (_) #t))
+           ((pair? packages-to-process)
+            (lambda (package)
+              (find (lambda (name)
+                      (equal? name (ros-package-name package)))
+                    packages-to-process)))
+           (else (error "You must specify at least one ROS package to convert.")))))
 
-    (when (not (pair? packages-to-convert))
-      (error "You must specify at least one ROS package to convert."))
     (when (not ros-distro)
       (error "You must specify a ROS distro."))
 
-    (format #t "Using ROS distro ~a\n" ros-distro)
-    (format #t "ROS packages: ~a\n" packages-to-convert)
-
     (let* ((distribution-cache (fetch-distribution-cache ros-distro))
            (ros-packages (distribution-cache->ros-packages distribution-cache))
-           (matching-ros-packages (filter
-                                   (lambda (package)
-                                     (find (lambda (name)
-                                             (equal? name (ros-package-name package)))
-                                           packages-to-convert))
-                                   ros-packages))
-           (guix-packages (map create-guix-package matching-ros-packages)))
+           (matching-ros-packages (filter packages-pred ros-packages))
+           (guix-packages (map create-guix-package matching-ros-packages))
+           (imported-modules (delete-duplicates!
+                              (fold (lambda (package prev)
+                                      (append (guess-package-imports package) prev))
+                                    %default-modules
+                                    guix-packages))))
 
-      (pretty-print guix-packages))))
+      (pretty-print
+       `(define-module (ros ,(string->symbol ros-distro))
+          ,@(apply append (map (lambda (m) `(#:use-module ,m)) imported-modules))))
+      (for-each (lambda (package)
+                  (newline)
+                  (pretty-print package))
+                guix-packages))))
+
+(define %default-modules
+  '(((guix licenses) #:prefix license:)
+    (guix git-download)
+    (guix packages)))
 
 (define (fetch-distribution-cache ros-distro)
   (let* ((index (fetch-ros-index))
@@ -163,7 +183,8 @@ Convert the given PACKAGES.\n")
                         (string-replace-substring release-tag "{package}" package-name)
                         "{version}"
                         version))
-                  (xml (assoc-ref packages-xmls package-name)))
+                  (xml (assoc-ref packages-xmls package-name))
+                  (xml->sxml xml))
              (make-ros-package
               package-name
               url
@@ -177,14 +198,20 @@ Convert the given PACKAGES.\n")
         '())))
 
 (define (create-guix-package ros-package)
-  (let* ((sxml (xml->sxml (ros-package-xml ros-package)))
-         ;(_ (pretty-print sxml))
-         (package-name (ros-package-name ros-package))
-         (package-description (ros-package-xml-desc sxml))
-         (package-home-page (ros-package-xml-home-page ros-package sxml))
-         (package-license (ros-package-xml-license sxml))
-         (build-system (guess-build-system sxml))
-         (guix-license (ros-license->guix-license package-license)))
+  (define (format-package-def package-name)
+    `(list ,package-name ,(string->symbol package-name)))
+
+  (let*-values (((sxml) (xml->sxml (ros-package-xml ros-package)))
+                ((package-name) (ros-package-name ros-package))
+                ((package-description) (ros-package-xml-desc sxml))
+                ((package-home-page) (ros-package-xml-home-page ros-package sxml))
+                ((package-license) (ros-package-xml-license sxml))
+                ((build-system) (guess-build-system sxml))
+                ((guix-license) (ros-license->guix-license package-license))
+                ((native-inputs inputs propagated-inputs) (guess-package-dependencies sxml))
+                ((native-inputs) `(list ,@(map format-package-def native-inputs)))
+                ((inputs) `(list ,@(map format-package-def inputs)))
+                ((propagated-inputs) `(list ,@(map format-package-def propagated-inputs))))
 
     ;(pretty-print sxml)
 
@@ -199,13 +226,49 @@ Convert the given PACKAGES.\n")
                        (commit ,(ros-package-tag ros-package))
                        (file-name (git-file-name name version))))))
         (build-system ,build-system)
-        (native-inputs '())
-        (inputs '())
-        (propataged-inputs '())
+        (native-inputs ,native-inputs)
+        (inputs ,inputs)
+        (propagated-inputs ,propagated-inputs)
         (home-page ,package-home-page)
         (synopsis ,(format #f "ROS package ~a" package-name))
         (description ,package-description)
         (license ,guix-license)))))
+
+(define (guess-package-dependencies ros-package-sxml)
+  "Given a <ros-package>, return a three-sized list of native-inputs,
+inputs and propagated inputs guix-like names"
+
+  (define (ros-dep-to-guix dep)
+    "Given dep as a string, will return a guix-style dependency name"
+
+    (set! dep (string-replace-substring dep "_" "-"))
+    (set! dep (string-replace-substring dep "python3" "python"))
+
+    dep)
+
+  (define (is-propagated-dependency dep)
+    "Given a dep as a guix package name, return #t if it is a propagated dependency
+     (e.g. a scripting dependnecy)."
+
+    #f
+    ;;(string-prefix? "python" dep)
+    )
+
+  (define (remove-version dep) (car (last-pair dep)))
+
+  (define (get-cleaned-guix-dep proc)
+    (map ros-dep-to-guix
+         (map remove-version (proc ros-package-sxml))))
+
+  ;; We do not handle versions for now.
+  (let*-values (((propagated-build-dependencies build-dependencies)
+                 (partition! is-propagated-dependency (get-cleaned-guix-dep ros-package-xml-build-dependencies)))
+                ((propagated-dependencies dependencies)
+                 (partition! is-propagated-dependency (get-cleaned-guix-dep ros-package-xml-dependencies)))
+                ((run-dependencies)
+                 (append propagated-build-dependencies propagated-dependencies (get-cleaned-guix-dep ros-package-xml-run-dependencies))))
+
+    (values build-dependencies dependencies run-dependencies)))
 
 (define (guess-package-imports package-definition)
   (let* ((package (third package-definition))
@@ -213,10 +276,10 @@ Convert the given PACKAGES.\n")
          (native-inputs (car (assq-ref package 'native-inputs)))
          (inputs (car (assq-ref package 'inputs)))
          (propagated-inputs (car (assq-ref package 'propagated-inputs)))
-         (total-inputs (append (cadr native-inputs) (cadr inputs) (cadr propagated-inputs)))
-         (total-inputs-symbols (delete-duplicates! (map cadadr total-inputs) eq?)))
+         (total-inputs (append (cdr native-inputs) (cdr inputs) (cdr propagated-inputs)))
+         (total-inputs-symbols (delete-duplicates! (map caddr total-inputs) eq?)))
 
-    (delete-duplicates! (map m-symbol->module total-inputs-symbols) eq?)))
+    (delete-duplicates! (filter-map m-symbol->module total-inputs-symbols) eq?)))
 
 (define (symbol->module symbol)
   "Resolve symbol to a module path"
@@ -224,7 +287,9 @@ Convert the given PACKAGES.\n")
   (match (assq-ref hard-coded-symbol->module-map symbol)
     (#f
      (match (find-package-locations (symbol->string symbol))
-       (() (error "Could not find package location for " symbol))
+       (() #f
+        ;;(error "Could not find package location for " symbol)
+        )
        (locations
         (map string->symbol
              (string-split
@@ -247,8 +312,9 @@ Convert the given PACKAGES.\n")
   (let* ((build-type (first (ros-package-xml-exported-build-type sxml))))
 
     (cond
-     ((equal? build-type "ament_cmake") 'ament-build-system)
-     (else (error "Could not guess build type for package.xml:" sxml)))))
+     ((equal? build-type "ament_cmake") 'ament-cmake-build-system)
+     ((equal? build-type "ament_python") 'ament-python-build-system)
+     (else (error "Could not guess build type for package.xml:" sxml build-type)))))
 
 (define ros-package-xml-exported-build-type
   (sxpath '(package export build_type *text*)))
@@ -264,6 +330,22 @@ Convert the given PACKAGES.\n")
 
 (define (ros-package-xml-license sxml)
   (first ((sxpath '(package license *text*)) sxml)))
+
+(define ros-package-xml-build-dependencies
+  (sxpath `(package
+            ,(node-or
+              (sxpath '(buildtool_depend))
+              (sxpath '(build_depend))
+              (sxpath '(test_depend))))))
+
+(define ros-package-xml-dependencies
+  (sxpath '(package depend)))
+
+(define ros-package-xml-run-dependencies
+  (sxpath `(package
+            ,(node-or
+              (sxpath '(run_depend))
+              (sxpath '(exec_depend))))))
 
 (define (ros-license->guix-license ros-license)
   (let ((guix-license (assoc-ref ros-licenses-to-guix-assoc ros-license)))
